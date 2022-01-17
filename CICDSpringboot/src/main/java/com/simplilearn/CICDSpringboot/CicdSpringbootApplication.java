ed8aebd93e5f53bac5ce1b74c6015f6aa8d9da7f
Swathi K <swathi.kanduri7@gmail.com>
2022-01-17 19:02:24 +0530

    first commit

---
+++ CICDSpringboot/src/main/java/com/simplilearn/CICDSpringboot/CicdSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.CICDSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSpringbootApplication.class, args);
+	}
+
+}
---
+++ CICDSpringboot/src/test/java/com/simplilearn/CICDSpringboot/CicdSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.CICDSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
